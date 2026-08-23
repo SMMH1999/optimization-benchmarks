0cb4d3fc7d157c41565b5c83d4c4aca11522138a
smmh <osonhast@gmail.com>
2026-08-23 17:26:54 +0330

    Delete RW PS1

--- Engineering Problems/Problem Set1/RW_Function.m
+++
@@ -1,9 +0,0 @@
-function [LB, UB, Dim] = RW_Function(functionNo)
-% Returns bounds & dimension for RW problems, compatible with RunBenchmarkSuite.
-
-    [Dim, LB, UB] = ProbInfo(functionNo);
-
-    % Ensure row vectors (some algorithms prefer 1xD)
-    LB = LB(:).';
-    UB = UB(:).';
-end
